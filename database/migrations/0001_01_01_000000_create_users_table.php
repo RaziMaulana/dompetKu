efcd9575853d7d85df135dab1949f882a13a30f6
RaziMaulana <razifaturmaulana@gmail.com>
2025-03-16 06:40:31 +0800

    making database nullable -> done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone');
             $table->string('google_id')->nullable();
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
