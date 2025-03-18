797164a91354760029c07e4f2682df6d39b0f585
paturr123 <khafialalf@gmail.com>
2025-03-18 14:27:41 +0800

    modify dashboard

--- resources/views/dashboardNew.blade.php
+++ resources/views/dashboardNew.blade.php
@@ -16,7 +16,6 @@
             margin: 0 auto;
             background-color: white;
             height: 100vh;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
         /* Header */
@@ -24,7 +23,7 @@
             background-color: #1a1f2b;
             display: flex;
             align-items: center;
-            padding: 15px 20px;
+            padding: 15px 140px;
             justify-content: space-between;
             color: white;
         }
@@ -93,12 +92,14 @@
             align-items: center;
             justify-content: space-between;
             gap: 40px;
-            padding: 20px;
+            padding: 30px 140px;
         }
 
         /* Balance Card */
         .balance-card {
-            background-color: #1a1f2b;
+            background-image: url('{{ asset('image/background-saldo.png') }}');
+            background-size: cover;
+            background-position: center;
             border-radius: 10px;
             padding: 20px;
             color: white;
@@ -172,10 +173,8 @@
             border-radius: 16px;
             padding: 30px;
             box-shadow: 0 2px 10px rgba(0, 0, 0, 0.40);
-            width: 97.60%;
-            margin: 0 auto;
-            margin-top: 20px;
-
+            width: calc(100% - 280px);
+            margin: 10px 140px;
         }
 
         .activity-header {
@@ -327,39 +326,7 @@
 
     <div class="container">
         <!-- Header -->
-        <div class="header">
-            <div class="logo">
-                <img src="{{ asset('image/dompetKu-logo.png') }}" alt="Logo">
-            </div>
-            <div class="nav">
-                <a href="#" class="nav-item active">Beranda</a>
-                <a href="#" class="nav-item">Transfer</a>
-                <a href="#" class="nav-item">Aktivitas</a>
-                <a href="#" class="nav-item">Catatan</a>
-                <a href="#" class="nav-item">Investasi</a>
-            </div>
-            <div class="header-right">
-                <div class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-                    </svg>
-                </div>
-                <div class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <circle cx="12" cy="12" r="3"></circle>
-                        <path
-                            d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
-                        </path>
-                    </svg>
-                </div>
-                <div class="icon">Keluar</div>
-            </div>
-        </div>
+        @include('layouts.navbar')
 
         <!-- Banner -->
         <div class="banner">
